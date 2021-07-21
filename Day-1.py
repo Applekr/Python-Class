bd53b05107e7abaed37b8277a567c9721f210a70
Applekr <applekr59@gmail.com>
2021-07-21 15:40:25 +0900

    day-1

---
+++ Day-1.py
@@ -0,0 +1,74 @@
+# print(테스트) (오류 코드)
+
+# ㅁ재ㅑ덜;ㅑ멎;대ㅑㅓㄹ;ㅐㅑ멎;ㅐ댜러;먀저대ㅑ러ㅐㅁ;쟈ㅓㄷㄹ
+
+# print("테스트")
+# print('테스트')
+
+# print("당신의 나이는" + input("나이를 적어주세요: ") + "살입니다")
+# print(f"당신의 나이는 {input('나이를 적어주세요:')}살입니다")
+
+# [1, 2, 3, 4] # 수정됨: 리스트
+# (1, 2, 3, 4) # 수정 안 됨: 튜플
+
+# a = 1
+# b = "1"
+# c = [1, 2, 3]
+# d = (1, 2, 3)
+
+# str(1) # "1"
+# int("1") # 1
+
+# print(int(input("첫번쨰 수를 입력해주세요: ")) + int(input("두번째 수를 입력해주세요: ")))
+
+# a = int(input("첫 번쨰 수를 입력하세요: "))
+# b = int(input("두 번쨰 수를 입력하세요: "))
+# c = int(input("세 번쨰 수를 입력하세요: "))
+
+# d = a - b - c
+
+# print(d)
+
+# try:
+
+# while True:
+
+# for i in [1, 2, 3, 4, 5]:
+#     print(i - 1)
+
+# try:
+#     while True:
+#         a = input("+, -, *, /, stop중에 하나를 입력해주세요: ")
+
+#         if a == "+":
+#             print(int(input("첫번쨰 수를 입력해주세요: ")) + int(input("두번째 수를 입력해주세요: ")))
+#         elif a == "-":
+#             print(int(input("첫번쨰 수를 입력해주세요: ")) - int(input("두번째 수를 입력해주세요: ")))
+#         elif a == "*":
+#             print(int(input("첫번쨰 수를 입력해주세요: ")) * int(input("두번째 수를 입력해주세요: ")))
+#         elif a == "/":
+#             print(int(input("첫번쨰 수를 입력해주세요: ")) / int(input("두번째 수를 입력해주세요: ")))
+#         elif a == "stop":
+#             print("프로그램을 종료합니다")
+#             break
+#         else:
+#             print("글씨 좀 읽어")
+# except:
+#     print("오류")
+
+# try:
+#     a = int(input("4 * 3 ="))
+#     if a == 12:
+#         print("정답입니다")
+#     else:
+#         print("오답입니다.")
+# except:
+#     print("오류입니다.")
+
+# f = open("text.txt", "w")
+# f.write("ㅁㄴㅇㄹ")
+# f.close()
+
+# with open("text.txt", "r") as f:
+#     print(f.read())
+
